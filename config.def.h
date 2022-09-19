4ceaeae26b97717b210c2a78b525c553dca93ab9
thatguynoe <noereyesrivas@gmail.com>
2022-09-18 23:56:03 -0400

    apply the fonts2 patch

--- config.def.h
+++ config.def.h
@@ -6,6 +6,8 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "Liberation Mono:pixelsize=12:antialias=true:autohint=true";
+static char *font2[] = { "Joy Pixels:pixelsize=18:antialias=true:autohint=true" };
+
 static int borderpx = 2;
 
 /*
